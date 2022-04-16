40d6b3f7b7bfa2f1e7455d1bc8e8e2b62ff085ca
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-04-17 03:22:52 +0900

    62. Navigating with the "Link" Component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import Link from 'next/link';
+
 function HomePage() {
     return (
         <div>
             <h1>The Home Page</h1>
+            <ul>
+            <li>
+                    <a href="/portfolio">a tag - Portfolio</a>
+                </li>
+                <li>
+                    <Link replace href="/portfolio">link component - Portfolio</Link>
+                </li>
+                <li>
+                    <Link href="/clients">link component - Clients</Link>
+                </li>
+            </ul>
         </div>
     )
 }
