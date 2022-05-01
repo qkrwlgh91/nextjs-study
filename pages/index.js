7a13a1c0d5c7586f0869c1386caebde0e5f7156b
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-05-01 22:40:35 +0900

    92.Running Server-side Code & Using the Filesystem

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import path from 'path';
+// import fs from 'fs'; 
+import fs from 'fs/promises';
+
 function HomePage(props) {
   const { products } = props;
 
@@ -11,9 +15,14 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  // fs.readFileSync();
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: 'p1', title: 'Product 1' }],
+      products: data.products
     }
   }
 }
