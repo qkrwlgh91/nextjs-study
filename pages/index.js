76fa2e6589aa886319a9fe9317fcb317fa6980a1
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-05-07 19:02:17 +0900

    96. A Closer Look At "getStaticProps" Options

--- pages/index.js
+++ pages/index.js
@@ -14,18 +14,33 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   console.log('(Re-)Generating...');
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   // fs.readFileSync();
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true
+    };
+  }
+
   return {
     props: {
       products: data.products
     },
-    revalidate: 60
+    revalidate: 60,
+    notFound: false
   }
 }
 
