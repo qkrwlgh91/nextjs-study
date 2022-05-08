047ad43894d1f38c9d8f1dadf223ad19a180ab0e
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-05-08 21:22:49 +0900

    101. Working With Fallback Pages

--- pages/[pid].js
+++ pages/[pid].js
@@ -6,6 +6,10 @@ import { Fragment } from 'react';
 function ProductDetailPage(props) {
     const { loadedProduct } = props;
 
+    // if (!loadedProduct) {
+    //     return <p>Loading...</p>;
+    // }
+
     return (
         <Fragment>
             <h1>{loadedProduct.title}</h1>
@@ -37,10 +41,8 @@ export async function getStaticPaths() {
     return {
         paths: [
             { params: { pid: 'p1' } },
-            { params: { pid: 'p2' } },
-            { params: { pid: 'p3' } },
         ],
-        fallback: false
+        fallback: 'blocking' // true, false, blocking
     };
 }
 
--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export async function getStaticProps(context) {
     props: {
       products: data.products
     },
-    revalidate: 60,
+    revalidate: 10,
     notFound: false
   }
 }
