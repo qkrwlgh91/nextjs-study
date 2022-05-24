42c62335653f46aee3676d13268a447de142d896
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-05-25 00:03:20 +0900

    120. Optimizing Data Fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,20 +1,22 @@
 import { Fragment } from 'react';
 
-import { getAllEvents, getEventById } from '../../helpers/api-util';
+import { getFeaturedEvents, getEventById } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
-import { getStaticProps } from '..';
 
 function EventDetailPage(props) {
   const event = props.selectedEvent;
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      // <ErrorAlert>
+      //   <p>No event found!</p>
+      // </ErrorAlert>
+      <div className='center'>
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -42,18 +44,19 @@ export async function getStaticProps(context) {
   return {
     props: {
       selectedEvent: event
-    }
+    },
+    revalidate: 30
   }
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
 
   const paths = events.map(event => ({ params: { eventId: event.id } }));
 
   return {
     paths: paths,
-    fallback: false
+    fallback: true
   }
 }
 
