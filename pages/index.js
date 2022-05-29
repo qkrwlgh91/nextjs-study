fc7faf89206457c4cda300276e730f6facfbd137
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-05-29 17:47:21 +0900

    section8 init

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,9 @@
-import Link from 'next/link';
-
 function HomePage() {
-    return (
-        <div>
-            <h1>The Home Page</h1>
-            <ul>
-            <li>
-                    <a href="/portfolio">a tag - Portfolio</a>
-                </li>
-                <li>
-                    <Link replace href="/portfolio">link component - Portfolio</Link>
-                </li>
-                <li>
-                    <Link href="/clients">link component - Clients</Link>
-                </li>
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
 }
 
 export default HomePage;
