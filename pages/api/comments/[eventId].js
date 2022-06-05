5348d10c696c3aa9d6b4c2a85acf88944baeae10
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-06-06 03:16:14 +0900

    158. Inserting Comments Into The Database

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,9 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
     const eventId = req.query.eventId;
+
+    const client = await MongoClient.connect('mongodb+srv://nextjsDB:R03JFpAqjDd2rrQ7@cluster0.idtjo.mongodb.net/events?retryWrites=true&w=majority');
 
     if (req.method === 'POST') {
         const { email, name, text } = req.body;
@@ -17,13 +21,19 @@ function handler(req, res) {
         }
 
         const newComment = {
-            id: new Date().toISOString(),
+            eventId,
             email,
             name,
             text,
         };
 
-        console.log(newComment);
+        const db = client.db();
+
+        const result = await db.collection('comments').insertOne(newComment);
+
+        console.log(result);
+
+        newComment.id = result.insertedId;
 
         res.status(201).json({ message: 'Added comment.', comment: newComment });
     }
@@ -36,6 +46,8 @@ function handler(req, res) {
 
         res.status(200).json({ comments: dummyList });
     }
+
+    client.close();
 }
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -10,12 +10,12 @@ async function handler(req, res) {
         }
 
         const client = await MongoClient.connect(
-            'mongodb+srv://nextjsDB:R03JFpAqjDd2rrQ7@cluster0.idtjo.mongodb.net/newsletter?retryWrites=true&w=majority'
+            'mongodb+srv://nextjsDB:R03JFpAqjDd2rrQ7@cluster0.idtjo.mongodb.net/events?retryWrites=true&w=majority'
             );
       
         const db = client.db();
 
-        await db.collection('emails').insertOne({ email: userEmail });
+        await db.collection('newsletter').insertOne({ email: userEmail });
 
         client.close();
  
