cfeafee36802a4aa4b8b6397bd673e2e0534a9f1
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-06-11 22:27:22 +0900

    178. Getting Started With The Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function HomePage() {
+import { Fragment } from 'react';
 
+import Hero from '../components/home-page/hero';
+import FeaturedPosts from '../components/home-page/featured-posts';
+
+function HomePage() {
+    return (
+        <Fragment>
+            <Hero />
+            <FeaturedPosts />
+        </Fragment>
+    )
 }
 
 export default HomePage;
+
+// 1) Hero => Present ourselves
+// 2) Featured Posts
