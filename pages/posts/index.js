577b135bc0f1ef3db4883862055481f8e63d40c8
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-06-13 23:38:46 +0900

    185. Adding the "All Posts" Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,40 @@
-function AllPostsPage() {
+import AllPosts from "../../components/posts/all-posts";
 
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'Getting Started With NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
+        data: '2022-02-10',
+    },
+    {
+        slug: 'getting-started-with-nextjs2',
+        title: 'Getting Started With NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
+        data: '2022-02-10',
+    },
+    {
+        slug: 'getting-started-with-nextjs3',
+        title: 'Getting Started With NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
+        data: '2022-02-10',
+    },
+    {
+        slug: 'getting-started-with-nextjs4',
+        title: 'Getting Started With NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
+        data: '2022-02-10',
+    },
+];
+
+function AllPostsPage() {
+    return (
+        <AllPosts posts={DUMMY_POSTS} />
+    )
 }
 
 export default AllPostsPage;
