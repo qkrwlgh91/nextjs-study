afae68ecfa41926dae39ed97782bfefd9a1b60ff
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-06-14 22:55:25 +0900

    188. Adding Markdown Files As A Data Source

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -12,8 +12,7 @@ function PostItem(props) {
         year: 'numeric'
     });
 
-    // const imagePath = `/images/posts/${slug}/${image}`;
-    const imagePath = `/images/posts/${image}`;
+    const imagePath = `/images/posts/${slug}/${image}`;
     const linkPath = `/posts/${slug}`;
 
     return (
