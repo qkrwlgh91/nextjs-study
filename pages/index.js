597263ad895087875b49cdf8431a2729984ebb63
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-06-16 23:44:41 +0900

    190. Using Markdown Data For Rendering Posts

--- pages/index.js
+++ pages/index.js
@@ -3,44 +3,25 @@ import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
 
-const DUMMY_POSTS = [
-    {
-        slug: 'getting-started-with-nextjs',
-        title: 'Getting Started With NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
-        data: '2022-02-10',
-    },
-    {
-        slug: 'getting-started-with-nextjs2',
-        title: 'Getting Started With NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
-        data: '2022-02-10',
-    },
-    {
-        slug: 'getting-started-with-nextjs3',
-        title: 'Getting Started With NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
-        data: '2022-02-10',
-    },
-    {
-        slug: 'getting-started-with-nextjs4',
-        title: 'Getting Started With NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
-        data: '2022-02-10',
-    },
-];
+import { getFeaturedPosts } from '../lib/posts-util';
 
-function HomePage() {
+function HomePage(props) {
     return (
         <Fragment>
             <Hero />
-            <FeaturedPosts posts={DUMMY_POSTS} />
+            <FeaturedPosts posts={props.posts} />
         </Fragment>
     )
+}
+
+export function getStaticProps() {
+    const featuredPosts = getFeaturedPosts();
+
+    return {
+        props: {
+            posts: featuredPosts
+        }
+    }
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,40 +1,20 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-    {
-        slug: 'getting-started-with-nextjs',
-        title: 'Getting Started With NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
-        data: '2022-02-10',
-    },
-    {
-        slug: 'getting-started-with-nextjs2',
-        title: 'Getting Started With NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
-        data: '2022-02-10',
-    },
-    {
-        slug: 'getting-started-with-nextjs3',
-        title: 'Getting Started With NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
-        data: '2022-02-10',
-    },
-    {
-        slug: 'getting-started-with-nextjs4',
-        title: 'Getting Started With NextJS',
-        image: 'getting-started-nextjs.png',
-        excerpt: 'NextJS is a the React framework for production - it makes building fullstack~',
-        data: '2022-02-10',
-    },
-];
-
-function AllPostsPage() {
+function AllPostsPage(props) {
     return (
-        <AllPosts posts={DUMMY_POSTS} />
+        <AllPosts posts={props.posts} />
     )
+}
+
+export function getStaticProps() {
+    const allPosts = getAllPosts();
+
+    return {
+        props: {
+            posts: allPosts
+        }
+    }
 }
 
 export default AllPostsPage;
